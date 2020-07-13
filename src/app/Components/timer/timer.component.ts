515e5a44d69d844d454b3316b41e68aa25307200
Akshay Rathi <akshayrathi96@gmail.com>
2020-07-13 22:55:11 +0530

    Timer design fixed

--- src/app/Components/timer/timer.component.ts
+++ src/app/Components/timer/timer.component.ts
@@ -1,38 +1,36 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
-import { TaskService } from 'src/app/services/task.service';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from "@angular/core"
+import { TaskService } from "src/app/services/task.service"
 
 @Component({
-  selector: 'app-timer',
-  templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.css'],
+  selector: "app-timer",
+  templateUrl: "./timer.component.html",
+  styleUrls: ["./timer.component.css"],
 })
 export class TimerComponent implements OnInit, OnChanges {
   @Input() Timer: any
-  ringStroke:string;
+  ringStroke: string
   TaskName: string
   PopClass: boolean = false
-  constructor(private TaskArr: TaskService) {
-    
-  }
+  constructor(private TaskArr: TaskService) {}
   ngOnInit(): void {
     this.TaskName = this.Timer.task
   }
   ngOnChanges(changes: SimpleChanges) {
-    this.ringStroke = Math.floor(3.15*this.Timer.ring) + 'px, 315px'
+    this.ringStroke = Math.floor(3.15 * this.Timer.ring) + "px, 315px"
   }
-  TimerBtn(timer: string, id: string, status: boolean){
-    if(id === '-1'){
+  TimerBtn(timer: string, id: string, status: boolean) {
+    if (id === "-1") {
       this.PopUp(0)
-    }else{
+    } else {
       this.TaskArr.TaskSelector(id, timer, status)
     }
   }
-  PopUp(I: number){
-    if(I === 4){
+  PopUp(I: number) {
+    if (I === 4) {
       this.PopClass = false
-    }else{
+    } else {
       this.PopClass = true
-      setTimeout( () => this.PopUp(I+1) , 1000);
+      setTimeout(() => this.PopUp(I + 1), 1000)
     }
   }
 }
