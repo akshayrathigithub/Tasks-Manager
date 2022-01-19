5f22b7235b437546d1eaa2b4bb3b8f9107ce632b
Akshay Rathi <akshayrathi96@gmail.com>
2022-01-19 23:17:22 +0530

    backend integation of analytics component

--- src/app/Components/analytics/analytics.component.ts
+++ src/app/Components/analytics/analytics.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Chart } from "chart.js";
+import { PRIORITY, Task, TaskAnalytics, TASK_MANAGER_ANALTYICS } from "src/app/modules/taskModule";
+import { TaskService } from "src/app/services/task.service";
 
 @Component({
   selector: "app-analytics",
@@ -7,32 +9,44 @@ import { Chart } from "chart.js";
   styleUrls: ["./analytics.component.css"],
 })
 export class AnalyticsComponent implements OnInit {
-  constructor() {}
-  Selector: string = "Weekly";
+  Selector: string = "Yearly";
   toggle: boolean = false;
   ChartSelector: string = "doughnut";
   FilterStatus: string = "Filter";
   FilterIcon: string = "fas fa-chevron-down";
+  totalTasks: number = 0;
+  completedTasks: number = 0
+  inCompleteTasks: number = 0
+
+  constructor(private TaskArr: TaskService) {}
+
   ngOnInit(): void {
-    this.Chart(this.ChartSelector);
+    this.analytics()
   }
   Chart(chart: string) {
     const myChart = new Chart("myChart", {
       type: chart,
+      options: {
+          legend: {
+            display: false
+          }
+      },
       data: {
+        labels: [
+          'Completed Task',
+          'Incomplete Task',
+        ],
         datasets: [
           {
-            label: "# of Votes",
-            data: [15, 19, 30],
+            label: "# of Tasks",
+            data: [this.completedTasks, this.inCompleteTasks],
             backgroundColor: [
               "rgba(255, 99, 132, 1)",
               "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
             ],
             borderColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
             ],
             borderWidth: 1,
           },
@@ -42,6 +56,7 @@ export class AnalyticsComponent implements OnInit {
   }
   SelectorCalled(name: string) {
     this.Selector = name;
+    this.analytics()
   }
   ChartSelectorCalled(name: string) {
     this.ChartSelector = name;
@@ -54,5 +69,30 @@ export class AnalyticsComponent implements OnInit {
     this.FilterStatus = filter;
     this.FilterIcon = icon;
     this.toggle = !this.toggle;
+    this.analytics()
+  }
+  analytics(){
+    let parameter: TASK_MANAGER_ANALTYICS = TASK_MANAGER_ANALTYICS.YEARLY
+    if(this.Selector === "Monthly"){
+      parameter = TASK_MANAGER_ANALTYICS.MONTHLY
+    } else if(this.Selector === "Weekly"){
+      parameter = TASK_MANAGER_ANALTYICS.WEEKLY
+    }
+    
+    let priority = PRIORITY.ALL
+    if(this.FilterStatus === "High"){
+      priority = PRIORITY.HIGH
+    }else if(this.FilterStatus === "Medium"){
+      priority = PRIORITY.MEDIUM
+    }else if(this.FilterStatus === "Low"){
+      priority = PRIORITY.LOW
+    }
+
+    this.TaskArr.analytics(parameter, priority).subscribe((res: TaskAnalytics) => {
+    this.totalTasks = res.totalTasks
+    this.completedTasks = res.completedTasks
+    this.inCompleteTasks = res.inCompleteTasks
+    this.Chart(this.ChartSelector);  
+    })
   }
 }
