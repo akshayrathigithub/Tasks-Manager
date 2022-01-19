ee8b5f1f21d94e563a39abff7076f54f45c27e55
Akshay Rathi <akshayrathi96@gmail.com>
2022-01-19 23:18:03 +0530

    backend integation of prev task component

--- src/app/Components/prev-tasks/prev-tasks.component.ts
+++ src/app/Components/prev-tasks/prev-tasks.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Task } from 'src/app/modules/taskModule';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { PRIORITY, Task } from 'src/app/modules/taskModule';
 import { TaskService } from 'src/app/services/task.service';
 
 @Component({
@@ -11,16 +13,29 @@ export class PrevTasksComponent implements OnInit {
 
   Rows: number
   ActiveTask: string = ''
-  SearchedTaskName: string
+  SearchedTaskName: string = ''
   toggle: boolean = false
   FilterStatus: string = "Filter"
   FilterIcon: string = "fas fa-chevron-down"
   @Input() Tasks: Task[];
+  High = "fas fa-arrow-alt-circle-up"
+  Med = "fas fa-arrow-alt-circle-right"
+  Low = "fas fa-arrow-alt-circle-down"
+  textChanged: Subject<string> = new Subject<string>();
+  prevTasksList: Task[]
   
-  constructor(private TaskArr: TaskService) { }
+  constructor(private TaskArr: TaskService) {
+    this.textChanged.pipe(
+      debounceTime(300), 
+      distinctUntilChanged())
+      .subscribe(text => {
+        this.filterTasks(text)
+      });
+   }
 
   ngOnInit(): void {
     this.Rows = 1+this.Tasks.length
+    this.prevTasksList = [...this.Tasks]
   }
   Span(index: number){
     return `${index+2}/1/${index+3}/5`
@@ -32,8 +47,32 @@ export class PrevTasksComponent implements OnInit {
     this.FilterStatus = filter
     this.FilterIcon = icon
     this.toggle = !this.toggle
+    this.filterTasks(this.SearchedTaskName)
   }
   Delete(ID: string){
     this.TaskArr.getTaskDeleted(ID)
   }
+
+  get PRIORITY(){
+    return PRIORITY
+  }
+
+  inputChanged(text: string){
+    this.textChanged.next(text);
+  }
+
+  filterTasks = (searchTerm: string) =>{
+    let priority = PRIORITY.ALL
+    if(this.FilterStatus === "High"){
+      priority = PRIORITY.HIGH
+    }else if(this.FilterStatus === "Medium"){
+      priority = PRIORITY.MEDIUM
+    }else if(this.FilterStatus === "Low"){
+      priority = PRIORITY.LOW
+    }
+    this.TaskArr.filterTask(searchTerm, priority).subscribe((res: Task[]) => {
+      this.prevTasksList = [...res]
+    })
+  }
+
 }
